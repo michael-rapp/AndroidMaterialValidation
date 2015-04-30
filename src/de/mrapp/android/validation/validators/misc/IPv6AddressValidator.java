59ccb4537f04a9c79c6d0199dabaf959ec2477f4
michael <michael@localhost>
2015-04-30 14:33:59 +0200

    Added the class IPv6AddressValidator.

---
+++ src/de/mrapp/android/validation/validators/misc/IPv6AddressValidator.java
@@ -0,0 +1,82 @@
+/*
+ * AndroidMaterialValidation Copyright 2015 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
+package de.mrapp.android.validation.validators.misc;
+
+import java.util.regex.Pattern;
+
+import android.content.Context;
+import de.mrapp.android.validation.Validators;
+import de.mrapp.android.validation.validators.DisjunctiveValidator;
+
+/**
+ * A validator, which allows to validate texts to ensure, that they represent
+ * valid IPv6 addresses.
+ * 
+ * @author Michael Rapp
+ *
+ * @since 1.0.0
+ */
+public class IPv6AddressValidator extends DisjunctiveValidator<CharSequence> {
+
+	/**
+	 * A regular expression, which matches standard IPv6 addresses.
+	 */
+	private static final Pattern STANDARD_REGEX = Pattern
+			.compile("^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$");
+
+	/**
+	 * A regular expression, which matches compressed IPv6 addresses.
+	 */
+	private static final Pattern COMPRESSED_REGEX = Pattern
+			.compile("^((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)$");
+
+	/**
+	 * Creates a new validator, which allows to validate texts to ensure, that
+	 * they represent valid IPv6 addresses.
+	 * 
+	 * @param errorMessage
+	 *            The error message, which should be shown, if the validation
+	 *            fails, as an instance of the type {@link CharSequence}. The
+	 *            error message may not be null
+	 */
+	public IPv6AddressValidator(final CharSequence errorMessage) {
+		super(errorMessage, Validators.regex(errorMessage, STANDARD_REGEX),
+				Validators.regex(errorMessage, COMPRESSED_REGEX));
+	}
+
+	/**
+	 * Creates a new validator, which allows to validate texts to ensure, that
+	 * they represent valid IPv6 addresses.
+	 * 
+	 * @param context
+	 *            The context, which should be used to retrieve the error
+	 *            message, as an instance of the class {@link Context}. The
+	 *            context may not be null
+	 * @param resourceId
+	 *            The resource ID of the string resource, which contains the
+	 *            error message, which should be set, as an {@link Integer}
+	 *            value. The resource ID must correspond to a valid string
+	 *            resource
+	 */
+	public IPv6AddressValidator(final Context context, final int resourceId) {
+		super(context, resourceId, Validators.regex(context, resourceId,
+				STANDARD_REGEX), Validators.regex(context, resourceId,
+				COMPRESSED_REGEX));
+	}
+
+}
